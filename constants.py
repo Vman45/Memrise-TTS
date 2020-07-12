f690dad198e5c4b821731b12b2ac853fd5b8406a
Adam Cottis <adam.cottis@outlook.com>
2020-07-12 20:41:08 +0100

    Proof of concept for posting TTS files

--- constants.py
+++ constants.py
@@ -1,3 +1,5 @@
+DEBUG = False
+
 class Speed:
     """Read Speed
     """
@@ -12,12 +14,26 @@ TRANSLATE_INIT = 'https://papago.naver.com/apis/tts/makeID'
 TRANSLATE_ENDPOINT = 'https://papago.naver.com/apis/tts/{id}'
 
 NAVER_TTS_HEADERS = {
-    "Referer": "http://papago.naver.com/",
-    "User-Agent":
-        "Mozilla/5.0 (Windows NT 10.0; WOW64) "
-        "AppleWebKit/537.36 (KHTML, like Gecko) "
-        "Chrome/47.0.2526.106 Safari/537.36"
+        "Referer": "http://papago.naver.com/",
+        "User-Agent":
+                "Mozilla/5.0 (Windows NT 10.0; WOW64) "
+                "AppleWebKit/537.36 (KHTML, like Gecko) "
+                "Chrome/47.0.2526.106 Safari/537.36"
 }
+
+MEMRISE_HEADERS = {
+        #"Authority": "www.memrise.com",
+        #"Origin": "http://www.memrise.com",
+        "DNT" : "1",
+        "Referer" : "https://www.memrise.com/course/{course}",
+        "User-Agent": 
+                "Mozilla/5.0 (Windows NT 10.0; Win64; x64)" 
+                "AppleWebKit/537.36 (KHTML, like Gecko)" 
+                "Chrome/83.0.4103.116 Safari/537.36"
+}
+
+MEMRISE_COURSE_URL = "https://www.memrise.com/course/"
+MEMRISE_LOGIN_URL = "https://www.memrise.com/login/"
 
 LANGUAGES = {
         'en': 'English',
--- scrape_memorise.py
+++ scrape_memorise.py
@@ -3,11 +3,10 @@ from lxml import html
 import requests
 import re
 
+import constants
+
 # change this to your memorise username and password
 from settings import MEMORISE_USERNAME, MEMORISE_PASSWORD
-
-LOGIN_URL = "https://www.memrise.com/login/"
-
 
 
 class Scrape_Memorise:
@@ -15,26 +14,40 @@ class Scrape_Memorise:
     def __init__(self):
         self.session = requests.Session()
         self.words = []
+        self.headers = constants.MEMRISE_HEADERS
+        self.login_url = constants.MEMRISE_LOGIN_URL
+        self.course_url = constants.MEMRISE_COURSE_URL
+
+        if constants.DEBUG == True:
+            self.verify = r"FiddlerRoot.pem"
+        else:
+            self.verify = True
+
+        self.authenticity_token = ""
+
+        self.course = "5757638/daneoneun-sinaessi-wihae-baeugi-pilyohaeyo/edit/"
 
         #Main
-        self.login()
-        self.scrape()
-        self.write_to_file()
+        # self.login()
+        # self.scrape()
+        # self.write_to_file()
 
     def login(self):
         
-        login_page_html = self.session.get(LOGIN_URL)
-
+        #login_page_html = self.session.get(self.login_url)
+        login_page_html = self.session.get(self.login_url, verify=self.verify)
         tree = html.fromstring(login_page_html.text)
-        authenticity_token = list(set(tree.xpath("//input[@name='csrfmiddlewaretoken']/@value")))[0]
+        self.authenticity_token = list(set(tree.xpath("//input[@name='csrfmiddlewaretoken']/@value")))[0]
+        print(self.authenticity_token)
         payload = {'username': MEMORISE_USERNAME, 
                   'password':MEMORISE_PASSWORD,
-                  'csrfmiddlewaretoken':authenticity_token}
+                  'csrfmiddlewaretoken':self.authenticity_token}
 
-        self.session.post(LOGIN_URL, data = payload, headers = dict(referer=LOGIN_URL))
+        #self.session.post(self.login_url, data = payload, headers = dict(referer=self.login_url))
+        self.session.post(self.login_url, data = payload, headers = dict(referer=self.login_url), verify=self.verify)
 
     def scrape(self):
-        res = self.session.get('https://www.memrise.com/course/5757638/daneoneun-sinaessi-wihae-baeugi-pilyohaeyo/')
+        res = self.session.get('https://www.memrise.com/course/5757638/daneoneun-sinaessi-wihae-baeugi-pilyohaeyo/', verify=self.verify)
 
         page = BeautifulSoup(res.content, 'html.parser')
         for row in page.find_all('div',attrs={"class" : "col_a col text"}):
@@ -43,12 +56,38 @@ class Scrape_Memorise:
     def write_to_file(self):
         with open("wordlist.txt", "w", encoding="utf-8") as f:
             f.writelines(self.words)
+    
+    def post_audio(self):
+        edit = self.session.get("https://www.memrise.com/course/5757638/daneoneun-sinaessi-wihae-baeugi-pilyohaeyo/edit/")
+
+        csrftoken = edit.cookies['csrftoken']
+        print(csrftoken)
+        self.headers['Referer'] = self.course_url + self.course
+
+        files = {
+            'f':  ('안녕하세요.mp3', open("AudioFiles/안녕하세요.mp3", "rb"), 'audio/mpeg'),
+        }
+
+        data = {
+            'thing_id': (None,256276441),
+            'cell_id': (None,3),
+            'cell_type': (None,"column"),
+            'csrfmiddlewaretoken': (None, csrftoken),
+        }
 
 
+        ##########################
+############################
+
+        test = self.session.post(url="https://www.memrise.com/ajax/thing/cell/upload_file/", headers=self.headers, data=data, files=files, verify=self.verify)
+        print(test.status_code)
 
 
-Scrape_Memorise()
-
+sm = Scrape_Memorise()
+sm.login()
+sm.scrape()
+sm.post_audio()
+#sm.write_to_file()
 
 # Navigate to the next page and scrape the data
 
